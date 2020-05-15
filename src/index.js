9f40ca54f31d58a4ec6739a963861c7d86da0698
adiwaru <65328434+adiwaru@users.noreply.github.com>
2020-05-15 13:55:45 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import 'regenerator-runtime';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './script/component/web-bar.js';
+import './script/component/global-data.js';
 import './script/data/data-source.js';
