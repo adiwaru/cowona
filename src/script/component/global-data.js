66cd3f5c295e7948f9eea127c52259f466963070
adiwaru <65328434+adiwaru@users.noreply.github.com>
2020-05-18 10:10:05 +0800

    Add files via upload

--- src/script/component/global-data.js
+++ src/script/component/global-data.js
@@ -6,17 +6,15 @@ class GlobalData extends HTMLElement {
   render()
   {
     this.innerHTML = `
-<div class="container">
+    <div class="container">
      <div class="row mt-3">
         <div class="col">
          <h2>Pantau COVID19 - Data Global</h2>
         </div>
     </div>
-
     <div class="row mt-3">
     <div class="col">
     <div class="row">
-
       <div class="col">
         <div class="card text-white bg-primary mb-3">
           <div class="card-body">
@@ -26,17 +24,6 @@ class GlobalData extends HTMLElement {
           </div>
         </div>
       </div>
-
-      <div class="col">
-        <div class="card text-white bg-success mb-3">
-          <div class="card-body">
-            <h5 class="card-title">Total Sembuh</h5>
-            <h5 class="card-title"><div id="reco"></div></h5>
-            <p class="card-text">Orang</p>
-          </div>
-        </div>
-      </div>
-
       <div class="col">
         <div class="card text-white bg-danger mb-3">
           <div class="card-body">
@@ -46,7 +33,15 @@ class GlobalData extends HTMLElement {
           </div>
         </div>
       </div>
-
+      <div class="col">
+        <div class="card text-white bg-success mb-3">
+          <div class="card-body">
+            <h5 class="card-title">Total Sembuh</h5>
+            <h5 class="card-title"><div id="reco"></div></h5>
+            <p class="card-text">Orang</p>
+          </div>
+        </div>
+      </div>
     </div>
     </div>
     </div>
